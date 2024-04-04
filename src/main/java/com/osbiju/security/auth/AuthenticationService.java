4db967f4130b973d7001e4021d5d71cb59f2dc8f
Oscar <okeshik@protonmail.com>
2024-04-04 12:13:20 +0200

    creo los request

---
+++ src/main/java/com/osbiju/security/auth/AuthenticationService.java
@@ -0,0 +1,12 @@
+package com.osbiju.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+
+
+}
